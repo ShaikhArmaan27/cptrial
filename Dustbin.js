a9df141e1fdc266d694e9daaefa98a450c8ef1d0
ShaikhArmaan27 <72267363+ShaikhArmaan27@users.noreply.github.com>
2020-11-07 08:14:42 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,18 @@
+class Dustbin
+
+{ 
+constructor(line1,line2,line3,height) {
+    var options = {
+        isStatic: true
+    }
+    
+    line1 = createSprite(330,620,20,100)
+	line2 = createSprite(390,650,100,20)
+	line3 = createSprite(450,620,20,100)
+ 
+}
+    
+	
+
+
+}
